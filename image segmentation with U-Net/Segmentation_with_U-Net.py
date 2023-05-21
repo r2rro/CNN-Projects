758f9b9dc5e8b4cf4566fd03fe83372f7bf83920
Arthur <96370575+r2rro@users.noreply.github.com>
2023-05-21 16:02:59 -0700

    Create Segmentation_with_U-Net.py

---
+++ image segmentation with U-Net/Segmentation_with_U-Net.py
@@ -0,0 +1,253 @@
+'''
+A U-Net implementation for semantic image segmentation on the CLARA self-driving car dataset.
+'''
+
+# importing the needed libraries:
+
+import os
+import tensorflow as tf
+import numpy as np
+import pandas as pd
+import imageio
+import matplotlib.pyplot as plt
+
+from tensorflow.keras.layers import Input
+from tensorflow.keras.layers import Conv2D
+from tensorflow.keras.layers import MaxPooling2D
+from tensorflow.keras.layers import Dropout 
+from tensorflow.keras.layers import Conv2DTranspose
+from tensorflow.keras.layers import concatenate
+
+from test_utils import summary, comparator
+
+
+# loading and splitting the data:
+
+path = ''
+image_path = os.path.join(path, './data/CameraRGB/')
+mask_path = os.path.join(path, './data/CameraMask/')
+image_list_orig = os.listdir(image_path)
+image_list = [image_path+i for i in image_list_orig]
+mask_list = [mask_path+i for i in image_list_orig]
+
+
+# splitting data into unmasked and masked images:
+
+image_list_ds = tf.data.Dataset.list_files(image_list, shuffle=False)
+mask_list_ds = tf.data.Dataset.list_files(mask_list, shuffle=False)
+
+for path in zip(image_list_ds.take(3), mask_list_ds.take(3)):
+    print(path)
+image_filenames = tf.constant(image_list)
+masks_filenames = tf.constant(mask_list)
+
+dataset = tf.data.Dataset.from_tensor_slices((image_filenames, masks_filenames))
+
+for image, mask in dataset.take(1):
+    print(image)
+    print(mask)
+
+    
+# pre-processing data
+
+def process_path(image_path, mask_path):
+    img = tf.io.read_file(image_path)
+    img = tf.image.decode_png(img, channels=3)
+    img = tf.image.convert_image_dtype(img, tf.float32)
+
+    mask = tf.io.read_file(mask_path)
+    mask = tf.image.decode_png(mask, channels=3)
+    mask = tf.math.reduce_max(mask, axis=-1, keepdims=True)
+    return img, mask
+
+def preprocess(image, mask):
+    input_image = tf.image.resize(image, (96, 128), method='nearest')
+    input_mask = tf.image.resize(mask, (96, 128), method='nearest')
+
+    return input_image, input_mask
+
+image_ds = dataset.map(process_path)
+processed_image_ds = image_ds.map(preprocess)
+
+
+# U-Net
+
+def conv_block(inputs=None, n_filters=32, dropout_prob=0, max_pooling=True):
+    """
+    Convolutional downsampling block
+    
+    Arguments:
+        inputs -- Input tensor
+        n_filters -- Number of filters for the convolutional layers
+        dropout_prob -- Dropout probability
+        max_pooling -- Use MaxPooling2D to reduce the spatial dimensions of the output volume
+    Returns: 
+        next_layer, skip_connection --  Next layer and skip connection outputs
+    """
+    conv = Conv2D(filters = n_filters, # Number of filters
+                  kernel_size = 3,   # Kernel size   
+                  activation='relu',
+                  padding='same',
+                  kernel_initializer='he_normal')(inputs)
+    conv = Conv2D(filters = n_filters, # Number of filters
+                  kernel_size = 3,   # Kernel size
+                  activation='relu',
+                  padding='same',
+                  kernel_initializer='he_normal')(conv)
+    
+    if dropout_prob > 0:
+        conv = Dropout(dropout_prob)(conv)
+         
+    if max_pooling:
+        next_layer = MaxPooling2D((2,2))(conv)
+        
+    else:
+        next_layer = conv
+        
+    skip_connection = conv
+    
+    return next_layer, skip_connection
+  
+  def upsampling_block(expansive_input, contractive_input, n_filters=32):
+    """
+    Convolutional upsampling block
+    
+    Arguments:
+        expansive_input -- Input tensor from previous layer
+        contractive_input -- Input tensor from previous skip layer
+        n_filters -- Number of filters for the convolutional layers
+    Returns: 
+        conv -- Tensor output
+    """
+    
+    up = Conv2DTranspose(
+                 filters = n_filters,
+                 kernel_size = 3,
+                 strides=(2,2),
+                 padding='same')(expansive_input)
+    
+    # Merge the previous output and the contractive_input
+    merge = concatenate([up, contractive_input], axis=3)
+    conv = Conv2D(filters = n_filters,
+                 kernel_size = 3,
+                 activation='relu',
+                 padding='same',
+                 kernel_initializer='he_normal')(merge)
+    conv = Conv2D(filters = n_filters,  # Number of filters
+                 kernel_size = 3,
+                 activation='relu',
+                 padding='same',
+                 kernel_initializer='he_normal')(conv)
+    
+    return conv
+  
+  def unet_model(input_size=(96, 128, 3), n_filters=32, n_classes=23):
+    """
+    Unet model
+    
+    Arguments:
+        input_size -- Input shape 
+        n_filters -- Number of filters for the convolutional layers
+        n_classes -- Number of output classes
+    Returns: 
+        model -- tf.keras.Model
+    """
+    inputs = Input(input_size)
+    # Contracting Path (encoding)
+    cblock1 = conv_block(inputs, n_filters)
+    cblock2 = conv_block(cblock1[0], 2 * n_filters)
+    cblock3 = conv_block(cblock2[0], 4 * n_filters)
+    cblock4 = conv_block(cblock3[0], 8 * n_filters, dropout_prob=0.3)
+    cblock5 = conv_block(cblock4[0], 16 * n_filters, dropout_prob=0.3, max_pooling=False) 
+    
+    # Expanding Path (decoding)
+    ublock6 = upsampling_block(cblock5[0], cblock4[1],  8 * n_filters)
+    ublock7 = upsampling_block(ublock6, cblock3[1],  4 * n_filters)
+    ublock8 = upsampling_block(ublock7, cblock2[1],  2 * n_filters)
+    ublock9 = upsampling_block(ublock8, cblock1[1],  n_filters)
+
+    conv9 = Conv2D(n_filters,
+                 3,
+                 activation='relu',
+                 padding='same',
+                 kernel_initializer='he_normal')(ublock9)
+
+    conv10 = Conv2D(filters = n_classes, kernel_size = (1,1), padding='same')(conv9)
+    
+    model = tf.keras.Model(inputs=inputs, outputs=conv10)
+
+    return model
+  
+# Set Model Dimensions:
+img_height = 96
+img_width = 128
+num_channels = 3
+unet = unet_model((img_height, img_width, num_channels))
+comparator(summary(unet), outputs.unet_model_output)
+
+# Model Summary:
+unet.summary()
+
+# Loss-Function:
+unet.compile(optimizer='adam',
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+
+# Dataset Handling
+def display(display_list):
+    plt.figure(figsize=(15, 15))
+
+    title = ['Input Image', 'True Mask', 'Predicted Mask']
+
+    for i in range(len(display_list)):
+        plt.subplot(1, len(display_list), i+1)
+        plt.title(title[i])
+        plt.imshow(tf.keras.preprocessing.image.array_to_img(display_list[i]))
+        plt.axis('off')
+    plt.show()
+    
+for image, mask in image_ds.take(1):
+    sample_image, sample_mask = image, mask
+    print(mask.shape)
+display([sample_image, sample_mask])
+
+for image, mask in processed_image_ds.take(1):
+    sample_image, sample_mask = image, mask
+    print(mask.shape)
+display([sample_image, sample_mask])
+
+# Train the Model:
+
+EPOCHS = 5
+VAL_SUBSPLITS = 5
+BUFFER_SIZE = 500
+BATCH_SIZE = 32
+train_dataset = processed_image_ds.cache().shuffle(BUFFER_SIZE).batch(BATCH_SIZE)
+print(processed_image_ds.element_spec)
+model_history = unet.fit(train_dataset, epochs=EPOCHS)
+
+# Create Predicted Masks:
+
+def create_mask(pred_mask):
+    pred_mask = tf.argmax(pred_mask, axis=-1)
+    pred_mask = pred_mask[..., tf.newaxis]
+    return pred_mask[0]
+  
+# Model Accuracy:
+plt.plot(model_history.history["accuracy"])
+  
+# Predictions:
+  
+def show_predictions(dataset=None, num=1):
+    """
+    Displays the first image of each of the num batches
+    """
+    if dataset:
+        for image, mask in dataset.take(num):
+            pred_mask = unet.predict(image)
+            display([image[0], mask[0], create_mask(pred_mask)])
+    else:
+        display([sample_image, sample_mask,
+             create_mask(unet.predict(sample_image[tf.newaxis, ...]))])
+        
+show_predictions(train_dataset, 6)
